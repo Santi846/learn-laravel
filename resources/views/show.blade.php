a2739733d8ff2a92a3be267c84cf9aa24642bc93
Santiago Arocha <arochas846@gmail.com>
2023-03-05 00:47:14 -0300

    Showing the data

---
+++ resources/views/show.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body {
+              background: rgb(48, 48, 102);
+              color: white;
+              font-family: Arial, Helvetica, sans-serif;
+              text-align: center;
+            }
+        div {
+           background: rgb(102, 102, 132);
+           border: solid 3px #000;
+           padding: 1.5%;
+           border-radius: 15px;
+           text-align: left
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Todo por id, que denso</h1>
+    <div>
+        {{ $pruebas }}
+    </div>
+</body>
+</html>
+
